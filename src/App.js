09cc8eaf231b889c5342187cf56d22472cfbb035
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-02 20:17:08 +0530

    feat: modify code to add a new user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
  import { useEffect, useState } from 'react';
 import './App.css';
-import { Button, EditableText, InputGroup } from '@blueprintjs/core';
+import { Button, EditableText, InputGroup, Toaster } from '@blueprintjs/core';
+
+// toaster is use to show notification
+const AppToaster = Toaster.create({
+  position: "top"
+})
 
 function App() {
   const[users, setUsers] = useState([]);
@@ -13,6 +18,41 @@ function App() {
     .then((response) => response.json())
     .then((json) => setUsers(json))
   },[]) 
+
+  function addUser() {
+    const newName = name.trim();
+    const newEmail = email.trim();
+    const newWebsite = website.trim();
+
+    if(newName && newEmail && newWebsite) {
+      fetch('http://jsonplaceholder.typicode.com/users',
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: newName,
+            email: newEmail,
+            website: newWebsite
+          }),
+          // header use to understand these data are json
+          // UTF-8 is encoding formate
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+          }
+        }
+      ).then((response) => response.json())
+        .then(json => {
+          setUsers([...users, json]);
+          AppToaster.show({
+            message: "User added successfully",
+            intent: 'success',
+            timeout: '3000'
+          })
+          setName("");
+          setEmail("");
+          setWebsite("");
+        })
+    }
+  }
 
   return (
     <div className="App">
@@ -39,6 +79,35 @@ function App() {
           )}
           
         </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>
+                <InputGroup 
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder='Enter Name'
+                />
+            </td>
+            <td>
+                <InputGroup 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder='Enter Email'
+                />
+            </td>
+            <td>
+                <InputGroup 
+                  value={website}
+                  onChange={(e) => setWebsite(e.target.value)}
+                  placeholder='Enter Website'
+                />
+            </td>
+            <td>
+              <Button intent='success' onClick={addUser}>Add User</Button>
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
